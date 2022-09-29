c1bb2580fc11c4b48d1882ca8b8ad5bbbe7b915f
mehedititu <mh.me.mehedi@gmail.com>
2022-09-29 23:42:49 +0600

    Final Check

--- src/components/ExerciseDetails/ExerciseDetails.js
+++ src/components/ExerciseDetails/ExerciseDetails.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './ExerciseDetails.css';
 
-// Toast
+// Toast Mehedi
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
