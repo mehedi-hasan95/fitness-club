70658f6271627b0d6876d61865064d9297397331
mehedititu <mh.me.mehedi@gmail.com>
2022-09-29 06:01:18 +0600

    Fitness image and info done

--- src/components/Fitness/Fitness.js
+++ src/components/Fitness/Fitness.js
@@ -6,6 +6,12 @@ const Fitness = (props) => {
     return (
         <div className='fitness'>
             <img src={img} alt="" />
+            <div className="fitness-info">
+                <h2>{title}</h2>
+                <p>{content}</p>
+                <strong>For Age: {age}</strong>
+                <p><strong>Time Required: {time}s</strong></p>
+            </div>
         </div>
     );
 };
